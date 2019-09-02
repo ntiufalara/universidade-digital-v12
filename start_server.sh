6164e11fb6ab5508f5d16532a9c40c848bcbc76c
marcosjfeneto <marcosjfneto@gmail.com>
2019-09-02 11:18:25 -0300

    Alteração do start_server.sh

--- start_server.sh
+++ start_server.sh
@@ -7,5 +7,5 @@ pip3 install -r requirements.txt
 if [[ ${ODOO_ENV} == 'dev' ]]; then
   /usr/bin/python3 /src/odoo-bin -u all -d odoo -r odoo -w odoo --db_host=127.0.0.1 --dev=all
 elif [[ ${ODOO_ENV} == 'prod' ]]; then
-  /usr/bin/python3 /src/odoo-bin -u all -d ud -r odoo -w odoo --db_host=127.0.0.1 --logfile=/src/access.log --logrotate
+  /usr/bin/python3 /src/odoo-bin -u all -d ud -r odoo -w odoo --db_host=127.0.0.1 --logfile=/src/access.log --logrotate --workers=2 --proxy-mode
 fi
