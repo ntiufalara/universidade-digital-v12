3b89770c80e10a235d2daeddeb3ca2bc078d70e9
marcosjfeneto <marcosjfneto@gmail.com>
2019-10-25 15:21:41 -0300

    alteração da pesquisa da pessoa pelo cpf

--- odoo/addons/ud_ru/controllers/controllers.py
+++ odoo/addons/ud_ru/controllers/controllers.py
@@ -111,23 +111,90 @@ class UdRu(http.Controller):
         })
 
 
+    @http.route('/ud_ru/almocar/', auth='public', website=True)
+    def almocar(self, **kw):
+        if kw.get('cpf'):
+            Pessoa = http.request.env['res.users']
+            pessoa = Pessoa.search([('cpf', '=', kw.get('cpf'))])
+
+            Refeicao_tipo = http.request.env['ud.ru.refeicao_tipo']
+            refeicao_tipo = Refeicao_tipo.search([('name', '=', 'Almoço')])  # ,('ativo','=',True)
+
+            valor_refeicao = 0
+
+            # ver o tipo de bolsa para fazer a comparação
+            # validar se já almoçou no dia
+
+            if len(pessoa.perfil_ids) > 1:
+                perfil = pessoa.perfil_principal
+                return "Almoçou - Tratar"
+            else:
+                perfil = pessoa.perfil_principal
+
+            if perfil == "Discente":
+                if (pessoa.perfil_ids.bolsista):  # TODO MUDAR A CONDIÇÃO PARA COMPARAR O TIPO DE BOLSA
+                    valor_refeicao = refeicao_tipo.valor_bolsista
+                else:
+                    valor_refeicao = refeicao_tipo.valor_aluno
+            elif perfil == "Técnico":
+                valor_refeicao = refeicao_tipo.valor_tecnico
+            elif perfil == "Docente":
+                valor_refeicao = refeicao_tipo.valor_docente
+
+            if pessoa.saldo < valor_refeicao:
+                return http.request.render('ud_ru.index', {
+                    'msg_alerta': "Saldo insuficiente para {}. Saldo atual é {}.".format(pessoa.name, pessoa.saldo),
+                })
+
+            # refeicao_id = http.request.env['ud.ru.refeicao'].create({
+            #     # 'name': refeicao_id,
+            #     'valor': valor_refeicao,
+            #     'tipo_refeicao_id': refeicao_tipo.name,
+            #     'pessoa_id': pessoa.id,
+            # })
+
+            movimentacao_id = http.request.env['ud.ru.movimentacao'].create({
+                # 'codigo': "A_{}".format(refeicao_id.id),
+                'valor': valor_refeicao,
+                'tipo': "almoco",
+                'pessoa_id': pessoa.id,
+                'funcionario_id': http.request.env.uid,
+            })
+
+            pessoa.decrementar_saldo(valor_refeicao)
+
+            return http.request.render('ud_ru.index', {
+                'msg_ok': "Foi debitado R$ {} de {}.".format(valor_refeicao, pessoa.name),
+            })
+        else:
+            return http.request.render('ud_ru.index')
 
 
-    @http.route('/ud_ru/get_saldo/', auth='user', methods=['get'])#, type="json"
+    @http.route('/ud_ru/get_uid/', auth='user', methods=['get'])  # , type="json"
+    def get_id(self, **kwargs):
+        _logger.info('GET UID:')
+        _logger.info(kwargs)
+        Pessoa = http.request.env['res.users']
+        usuario = Pessoa.search([('cpf', '=', kwargs.get('cpf'))])  #
+        # _logger.info(pessoa.name)
+        return json.dumps({"uid": usuario.id})
+
+    @http.route('/ud_ru/get_saldo/', auth='user', methods=['get'])  # , type="json"
     def get_saldo(self, **kwargs):
         _logger.info('GET SALDO:')
         _logger.info(kwargs)
-        _logger.info(http.request.env.uid)
-        Pessoa = http.request.env['res.users']
-        usuario = Pessoa.search([('id', '=', http.request.env.uid)]) #dessa forma não funciona com a apirest - passar ou o cpf ou o uid pela requisição
-        # _logger.info(pessoa.name)
-        return json.dumps({"saldo":usuario.saldo})
 
-    @http.route('/ud_ru/get_extrato/', auth='user', methods=['get'])#, type="json", auth='public',
+        Pessoa = http.request.env['res.users']
+        usuario = Pessoa.search([('cpf', '=', kwargs.get('cpf'))])  #
+        # _logger.info(pessoa.name)
+        return json.dumps({"saldo": usuario.saldo})
+
+    @http.route('/ud_ru/get_extrato/', auth='user', methods=['get'])  # , type="json", auth='public',
     def get_extrato(self, **kwargs):
         # https://www.odoo.com/documentation/12.0/reference/orm.html
         Movimentacao = http.request.env['ud.ru.movimentacao']
-        movimentacoes = Movimentacao.search([('pessoa_id', '=', http.request.env.uid)])#dessa forma não funciona com a apirest
+        movimentacoes = Movimentacao.search(
+            [('cpf', '=', kwargs.get('cpf'))])  #
         dados = []
 
         for movimentacao in movimentacoes:
@@ -139,16 +206,37 @@ class UdRu(http.Controller):
             }
             dados.append(mov)
 
-        return json.dumps(dados) #json.dumps(dados)#
+        return json.dumps(dados)  # json.dumps(dados)#
 
     @http.route('/ud_ru/get_nome/', auth='user', methods=['get'])
-    def get_nome(self, **kwargs):
+    def get_nome(self, id, **kwargs):
         Pessoa = http.request.env['res.users']
-        # _logger.info(kwargs.items())
-        # _logger.info(http.request)
-        pessoa = Pessoa.search([('id', '=', http.request.env.uid)])#dessa forma não funciona com a apirest
 
-        return json.dumps({"nome":pessoa.name})
+        pessoa = Pessoa.search([('cpf', '=', kwargs.get('cpf'))])  #
+
+        return json.dumps({"nome": pessoa.name})
+
+
+    # @http.route('/academy/<name>/', auth='public', website=True)
+    # def teacher(self, name):
+    #     return '<h1>{}</h1>'.format(name)
+
+    # @http.route('/academy/<int:id>/', auth='public', website=True)
+    # def teacher(self, id):
+    #     return '<h1>{} ({})</h1>'.format(id, type(id).__name__)
+
+    # @http.route('/ud_ru/ud_ru/objects/', auth='public')
+    # def list(self, **kw):
+    #     return http.request.render('ud_ru.listing', {
+    #         'root': '/ud_ru/ud_ru',
+    #         'objects': http.request.env['ud_ru.ud_ru'].search([]),
+    #     })
+
+#     @http.route('/ud_ru/ud_ru/objects/<model("ud_ru.ud_ru"):obj>/', auth='public')
+#     def object(self, obj, **kw):
+#         return http.request.render('ud_ru.object', {
+#             'object': obj
+#         })
 
 # class Utils(object):
 #         @staticmethod
@@ -180,134 +268,4 @@ class UdRu(http.Controller):
 #                 if resto < 2:
 #                     _dv1 = 0
 #                 return _dv1
-
-    # @http.route('/cadastrar/', auth='user', website=True)
-    # def cadastrar(self, **kw):
-    #
-    #     _logger.info(kw)
-    #
-    #     if kw.get('cpf'):
-    #         _logger.info("CADASTRADO")
-    #         pessoa_id = http.request.env['res.user'].create({
-    #             'name': kw.get('name'),
-    #             'cpf': kw.get('cpf'),
-    #             'rg': kw.get('rg'),
-    #             'e-mail': kw.get('e-mail'),
-    #
-    #
-    #         })
-    #         perfil_id = http.request.env['ud.perfil'].create({
-    #             'matricula': kw.get('matricula'),
-    #             'curso_id': kw.get('curso'),
-    #         })
-    #          #     kw['login'] = kw.get('cpf')
-    #         return http.request.render('ud_ru.cadastro', {
-    #             # 'grus': grus.search([]),
-    #         })
-    #     else:
-    #         _logger.info("nao CADASTRADO")
-    #         return http.request.render('ud_ru.cadastro', {
-    #             # 'grus': grus.search([]),
-    #         })
-        # obj_set = super(models.Model, self).create(vals)
-        # usuario_ud_group = self.env.ref('ud.usuario_ud')
-        # obj_set.groups_id |= usuario_ud_group
-        # return obj_set
-
-        # res = super(Responsavel, self).create(vals)
-        # group_gerente_servico = self.env.ref('ud_almoxarifado.group_ud_almoxarifado_gerente')
-        # res.pessoa_id.groups_id |= group_gerente_servico
-        # return res
-
-    @http.route('/ud_ru/almocar/', auth='public', website=True)
-    def almocar(self, **kw):
-        if kw.get('cpf'):
-            _logger.info("############################# PESSOA")
-            Pessoa = http.request.env['res.users']
-            pessoa = Pessoa.search([('cpf', '=', kw.get('cpf'))])
-
-            Refeicao_tipo = http.request.env['ud.ru.refeicao_tipo']
-            refeicao_tipo = Refeicao_tipo.search([('name', '=', 'Almoço')])#,('ativo','=',True)
-
-            valor_refeicao = 0
-
-            #ver o tipo de bolsa para fazer a comparação
-            #validar se já almoçou no dia
-
-
-
-
-            if len(pessoa.perfil_ids) > 1:
-                perfil = pessoa.perfil_principal
-                return "Almoçou - Tratar"
-            else:
-                perfil = pessoa.perfil_principal
-
-            if perfil == "Discente":
-                if (pessoa.perfil_ids.bolsista): #TODO MUDAR A CONDIÇÃO PARA COMPARAR O TIPO DE BOLSA
-                    valor_refeicao = refeicao_tipo.valor_bolsista
-                else:
-                    valor_refeicao = refeicao_tipo.valor_aluno
-            elif perfil == "Técnico":
-                valor_refeicao = refeicao_tipo.valor_tecnico
-            elif perfil == "Docente":
-                valor_refeicao = refeicao_tipo.valor_docente
-
-            if pessoa.saldo < valor_refeicao:
-                return http.request.render('ud_ru.index', {
-                    'msg_alerta': "Saldo insuficiente para {}. Saldo atual é {}.".format(pessoa.name, pessoa.saldo),
-                })
-
-            # refeicao_id = http.request.env['ud.ru.refeicao'].create({
-            #     # 'name': refeicao_id,
-            #     'valor': valor_refeicao,
-            #     'tipo_refeicao_id': refeicao_tipo.name,
-            #     'pessoa_id': pessoa.id,
-            # })
-
-            movimentacao_id = http.request.env['ud.ru.movimentacao'].create({
-                # 'codigo': "A_{}".format(refeicao_id.id),
-                'valor': valor_refeicao,
-                'tipo': "almoco",
-                'pessoa_id': pessoa.id,
-                'funcionario_id': http.request.env.uid,
-            })
-
-            pessoa.decrementar_saldo(valor_refeicao)
-            _logger.info(pessoa.saldo)
-
-            # Refeicao_tipo = http.request.env['ud.ru.refeicao_tipo']
-            # refeicao_tipo = Refeicao_tipo.search([()])
-            # pessoa.decrementar_saldo(2)
-
-            _logger.info("#############################")
-            return http.request.render('ud_ru.index', {
-                'msg_ok': "Foi debitado R$ {} de {}.".format(valor_refeicao,pessoa.name),
-            })
-        else:
-            return http.request.render('ud_ru.index')
-
-
-    # @http.route('/academy/<name>/', auth='public', website=True)
-    # def teacher(self, name):
-    #     return '<h1>{}</h1>'.format(name)
-
-    # @http.route('/academy/<int:id>/', auth='public', website=True)
-    # def teacher(self, id):
-    #     return '<h1>{} ({})</h1>'.format(id, type(id).__name__)
-
-#     @http.route('/ud_ru/ud_ru/objects/', auth='public')
-#     def list(self, **kw):
-#         return http.request.render('ud_ru.listing', {
-#             'root': '/ud_ru/ud_ru',
-#             'objects': http.request.env['ud_ru.ud_ru'].search([]),
-#         })
-
-#     @http.route('/ud_ru/ud_ru/objects/<model("ud_ru.ud_ru"):obj>/', auth='public')
-#     def object(self, obj, **kw):
-#         return http.request.render('ud_ru.object', {
-#             'object': obj
-#         })
-
-
 
