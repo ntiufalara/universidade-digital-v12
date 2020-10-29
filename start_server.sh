1c49df94bb483deda224be5ab021986d83b171b6
marcos <marcosjfneto@gmail.com>
2020-10-29 15:45:55 -0300

    Especificando o numero da porta do banco.

--- start_server.sh
+++ start_server.sh
@@ -5,7 +5,7 @@ ODOO_ENV=${ODOO_ENV:='prod'}
 pip3 install -r requirements.txt
 
 if [[ ${ODOO_ENV} == 'dev' ]]; then
-  /usr/bin/python3 /src/odoo-bin -u all -d odoo -r odoo -w odoo --db_host=127.0.0.1 --dev=all
+  /usr/bin/python3 /src/odoo-bin -u all -d odoo --db_port=5432 -r odoo -w odoo --db_host=127.0.0.1 --dev=all
 elif [[ ${ODOO_ENV} == 'prod' ]]; then
-  /usr/bin/python3 /src/odoo-bin -u all -d ud -r odoo -w odoo --db_host=127.0.0.1 --logfile=/src/access.log --logrotate --workers=2 --proxy-mode
+  /usr/bin/python3 /src/odoo-bin -u all -d ud --db_port=5432 -r odoo -w odoo --db_host=127.0.0.1 --logfile=/src/access.log --logrotate --workers=2 --proxy-mode
 fi
